2c5768be665c7f027dce56356d97579b11924593
Anthony-Wong <34021543+Anthony-Wong@users.noreply.github.com>
2019-04-19 11:34:43 +0800

    Add files via upload

---
+++ main/testScript.js
@@ -0,0 +1,19 @@
+var headings = document.evaluate("//td//td[contains(., 'https://ide.c9.io/candacefuller')]", document, null, XPathResult.ANY_TYPE, null );
+
+var thisHeading;
+var out = "";
+
+while (thisHeading = headings.iterateNext()) {
+    var position = thisHeading.parentElement.cells[3].innerHTML;
+    var name = thisHeading.parentElement.cells[4].innerHTML;
+
+    out = out + position + "\n" + name + "\n" + "Score : " + "\n" + "Remarks : " + "\n\n";
+}
+
+var dummy = document.createElement("textarea");
+document.body.appendChild(dummy);
+dummy.setAttribute("id", "dummy_id");
+document.getElementById("dummy_id").value=out;
+dummy.select();
+document.execCommand("copy");
+document.body.removeChild(dummy);
